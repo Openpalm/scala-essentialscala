53f2a8a825bff2ea6ffb9092157a6b8917a0be46
shokodemon <boo.factor@gmail.com>
2019-11-20 16:38:44 +0100

    monadic print

--- src/main/scala/Shapes.scala
+++ src/main/scala/Shapes.scala
@@ -6,39 +6,45 @@ object Shapes {
   }
 
   sealed trait Shape extends Printable {
-    def sides: Int
+    def name: String
+    def sides: Double 
     def perimeter: Double
     def area: Double
-    def putln: Option[Unit] = Some(println(s"i have $sides sides, making my perimeter $perimeter, and area $area"))
+    def putln: Option[Unit] = Some(println(s"i am a $name and i have $sides sides, making my perimeter $perimeter, and area $area"))
   }
+
   sealed trait Rectangular extends Shape {
     def sideA: Double 
     def sideB: Double
 
+    val name = "Rectangular Shape"
     val sides = 4
     val perimeter = 2 * (sideA + sideB)
     val area = sideA * sideB
   }
-  final case class Circle(radius: Long) extends Shape {
-    val sides = 0
+
+  final case class Circle(radius: Double) extends Shape {
+    val name = "Circle"
+    val sides = Double.PositiveInfinity
     val area = Pi * Math.pow(radius,2)
     val perimeter = (2*Pi*radius)
   }
 
-  final case class Rectangle(sideA: Double, sideB: Double) extends Rectangular
+  final case class Rectangle(sideA: Double, sideB: Double) extends Rectangular { 
+    override val name = "Rectangle"
+  }
   final case class Square(side: Double) extends Rectangular {
     val sideA, sideB = side
     override val area = Math.pow(side,2)
     override val perimeter =  4*side
-    override def putln: Option[Unit] = {
-      print("i am a square and ")
-      super.putln
-    }
   }
 
   def run { 
     for { 
-     _ <- Square(3).putln
+      _ <- Square(3).putln
+      _ <- Rectangle(3,5).putln
+      _ <- Circle(6.371).putln //earth in km.
+      _ <- Circle(695.510).putln //sun in km
     } yield ()
   }
 }
