496523e5601b9da2915fc5e418f1dc5cffbfb45f
shokodemon <boo.factor@gmail.com>
2019-11-22 09:10:40 +0100

    case classes

--- src/main/scala/Shapes.scala
+++ src/main/scala/Shapes.scala
@@ -1,11 +1,20 @@
 object Shapes { 
   import scala.math.Pi
 
+  // spice
   trait Printable { 
     def putln: Option[Unit]
   }
+ sealed  trait Color {
+    case class Color  (R: Int, G: Int, B: Int, V: Int = 0)
+    def color: Color
+    def isDark: Boolean = color.V > 50
+    def isLight: Boolean = !isDark
+  }
 
-  sealed trait Shape extends Printable {
+  // proginators 
+  sealed trait Shape extends Printable with Color {
+    val color: Color = Color(255, 255, 255) //white
     def name: String
     def sides: Double 
     def perimeter: Double
@@ -16,25 +25,25 @@ object Shapes {
   sealed trait Rectangular extends Shape {
     def sideA: Double 
     def sideB: Double
-
     val name = "Rectangular Shape"
     val sides = 4
     val perimeter = 2 * (sideA + sideB)
     val area = sideA * sideB
   }
 
+  // offspring
   final case class Circle(radius: Double) extends Shape {
     val name = "Circle"
     val sides = Double.PositiveInfinity
     val area = Pi * Math.pow(radius,2)
     val perimeter = (2*Pi*radius)
   }
-
   final case class Rectangle(sideA: Double, sideB: Double) extends Rectangular { 
     override val name = "Rectangle"
   }
   final case class Square(side: Double) extends Rectangular {
     val sideA, sideB = side
+    override val name = "Square"
     override val area = Math.pow(side,2)
     override val perimeter =  4*side
   }
@@ -43,10 +52,11 @@ object Shapes {
     for { 
       _ <- Square(3).putln
       _ <- Rectangle(3,5).putln
-      _ <- Circle(6.371).putln //earth in km.
-      _ <- Circle(695.510).putln //sun in km
+      _ <- Circle(6.371).putln //earth.
+      _ <- Circle(695.510).putln //sun.
     } yield ()
   }
+
 }
 
 Shapes.run
